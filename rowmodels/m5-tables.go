e468d9035618577efc6ca41c78beee36c8968ee6
Fred Baube <fbaube@iki.fi>
2024-01-20 09:03:49 +0200

    Do renamings

--- rowmodels/m5-tables.go
+++ rowmodels/m5-tables.go
@@ -2,8 +2,8 @@ package rowmodels
 
 import DRU "github.com/fbaube/datarepo/utils"
 
-// MmmcTableDetails configures the three key tables.
-var MmmcTableDetails = []DRU.TableDetails{
+// M5_TableDetails configures the three key tables.
+var M5_TableDetails = []DRU.TableDetails{
 	TableDetails_ContentityRow,
 	TableDetails_InbatchRow,
 	TableDetails_TopicrefRow,
