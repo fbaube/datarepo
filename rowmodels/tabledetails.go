6c71212ba0f40758fd375a93368188f861df16da
Fred Baube <fbaube@iki.fi>
2025-03-06 11:06:54 +0200

    WIP

--- rowmodels/tabledetails.go
+++ rowmodels/tabledetails.go
@@ -8,17 +8,18 @@ import(
 	D "github.com/fbaube/dsmnd"
 )
 
-// ColumnStringsCSV stores strings useful for composing 
-// SQL statements. Each string includes all the columns, 
-// in order, comma-separated. SQL using these strings
-// defaults to setting/getting every field in a DB record.
+// ColumnStringsCSV stores strings useful for composing SQL
+// statements. Each string includes all the columns, in order,
+// comma-separated. SQL using these strings defaults to setting
+// and getting every field in a DB record.
 //
-// The strings have no trailing commas. Each string has a 
-// "full" version (suffixed with "_wID") that includes the 
-// primary key (named "{table}_ID") for output from SELECT, 
-// and (importantly!) a version withOUT the "{table}_ID" 
-// primary key, for input to INSERT (where the ID is new)
-// and input to UPDATE (where the ID finds the record).
+// The strings have no trailing commas. Each string (except 
+// for UPDATE) has a "full" version (suffixed with "_wID")
+// that includes the primary key (always named "{table}_ID") 
+// for output from SELECT, and (importantly!) a version 
+// withOUT the "{table}_ID" primary key, for input to INSERT 
+// (where the ID is new) and input to UPDATE (where the ID
+// finds the record).
 // .
 type ColumnStringsCSV struct {
 	// FieldNames   [+withID primarykey] is a list of 
@@ -27,10 +28,10 @@ type ColumnStringsCSV struct {
 	// PlaceNumbers [+withID primarykey] is a list of 
 	// '$'-numbered parameters (like Postgres): "$1, $2, $3" 
 	PlaceNumbers,   PlaceNrs_wID    string
-	// FieldUpdates [+withID primarykey] is a list of
+	// FieldUpdates [NOT with primarykey] is a list of
 	// column/field names with "=", and the values as
 	// '$'-numbered parameters: "F1 = $1, F2 = $2, F3 = $3" 
-	UpdateNames,    UpdateNames_wID string
+	UpdateNames     string
 }
 
 // GenerateColumnStringsCSV generates struct [ColumnStringsCSV] 
@@ -51,7 +52,7 @@ func GenerateColumnStringsCSV(pTD *TableDetails) error {
      if pTD.CSVs != nil {
      	println("GenerateColumnStringsCSV: DUPE CALL")
 	return errors.New("GenerateColumnStringsCSV: dupe initialisation")
-	}
+	}    
      pTD.CSVs = new(ColumnStringsCSV)
      var colSpex []D.ColumnSpec
      colSpex   = pTD.ColumnSpecs
@@ -66,12 +67,24 @@ func GenerateColumnStringsCSV(pTD *TableDetails) error {
 	placeNumber = strconv.Itoa(i+1)
      	fmt.Printf("%s[%d]=%s: %s \n", pTD.StorName, i, columnName, pCS.String())
 	sbFN.WriteString(columnName  + ", ")
-	sbPN.WriteString(placeNumber + ", ")
+	sbPN.WriteString("$" + placeNumber + ", ")
 	sbUN.WriteString(columnName  + " = $" + placeNumber + ", ")
      }
-     println("FieldNames", sbFN.String())
-     println("PlaceNmbrs", sbPN.String())
-     println("UpdatNames", sbUN.String())
+     sFN := S.TrimSuffix(sbFN.String(), ", ")
+     sPN := sbPN.String()
+     sUN := S.TrimSuffix(sbUN.String(), ", ")
+     pTD.CSVs.FieldNames   = sFN
+     pTD.CSVs.PlaceNumbers = sPN
+     pTD.CSVs.UpdateNames  = sUN
+     pTD.CSVs.FieldNames_wID = pTD.PKname + ", " + sFN
+     pTD.CSVs.PlaceNrs_wID   = sPN + "$" + strconv.Itoa(len(colSpex)+1)
+     pTD.CSVs.PlaceNumbers   = S.TrimSuffix(pTD.CSVs.PlaceNumbers, ", ")
+     
+     println("FieldNames    ", pTD.CSVs.FieldNames)
+     println("FieldNames wID", pTD.CSVs.FieldNames_wID)
+     println("PlaceNmbrs    ", pTD.CSVs.PlaceNumbers)
+     println("PlaceNmbrs wID", pTD.CSVs.PlaceNrs_wID)
+     println("UpdatwNames   ", pTD.CSVs.UpdateNames)
      return nil
 }
 
